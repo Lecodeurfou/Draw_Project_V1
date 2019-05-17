b0af3d33283cf18fb564a05d6ad9e30401916803
Nness2 <menaa.aness2@gmail.com>
2019-05-17 17:32:23 +0100

    scene
    scene
    
    Co-Authored-By: lecodeurfou <lecodeurfou@users.noreply.github.com>

---
+++ Assets/personamove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personamove : MonoBehaviour {
+  
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 5;
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal*speed, 0.0f, moveVertical+speed);
+
+    }
+}
